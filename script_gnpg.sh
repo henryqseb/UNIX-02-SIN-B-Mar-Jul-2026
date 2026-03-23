89fc0c760f9b103ef384abb3249df9ea21675993
henryqseb <hequijiaso@uide.edu.ec>
2026-03-23 13:06:46 +0000

    doc no encryption

--- script_gnpg.sh
+++ script_gnpg.sh
@@ -3,3 +3,5 @@ gpg --armor --export-secret-keys
 gpg --armor --export chineado12@gmail.com > mi_llave_publica.asc
 gpg --list-keys
 gpg --import IsaakR_llave_publica.asc "Importar la llave de mi compañero"
+echo "este mensaje es secreto" > doc_no_cifrado.txt 
+"Crear un nuevo documento para que le llegue mi mensaje cifrado"
