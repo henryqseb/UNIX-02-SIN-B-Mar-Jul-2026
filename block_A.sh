be16a2e112381d0b71f25885ac4013c191fa2bdb
henryqseb <hequijiaso@uide.edu.ec>
2026-03-24 22:16:17 +0000

    Genearte the new keys

--- block_A.sh
+++ block_A.sh
@@ -2,3 +2,5 @@ gpg --version //It is used
 to display the current version of GnuPG
 which gpg //Display the path 
 of the executable file in the system
+gpg --full-generate-key //Genearte the
+new keys
