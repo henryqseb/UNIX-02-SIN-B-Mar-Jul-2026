df3b415a312b58b33f0fe4d526f99fad83e9c3f7
henryqseb <hequijiaso@uide.edu.ec>
2026-03-24 23:32:04 +0000

    Decrypt the file sent by my colleague

--- block_C.sh
+++ block_C.sh
@@ -2,3 +2,5 @@ echo "This is our Unix work" > doc_no_cifrado.txt
 //A file is created in which our message will be placed.
 gpg --output doc_cifrado.txt --encrypt --recipient XXXX doc_no_cifrado.txt
 //Generate the encrypted document to send to my colleague
+gpg --decrypt MICOMPA_doc_cifrado.txt
+// Decrypt the file sent by my colleague
