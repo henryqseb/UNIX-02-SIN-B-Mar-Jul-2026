82998723457658b90dddacc036776ca312246206
henryqseb <hequijiaso@uide.edu.ec>
2026-03-24 22:26:01 +0000

    Display a list of all my private keys and make them long

--- block_A.sh
+++ block_A.sh
@@ -5,3 +5,5 @@ of the executable file in the system
 gpg --full-generate-key //Genearte the
 new keys
 gpg --list-keys //List all our keys
+gpg --list-secret-keys --keyid-format=long // Display a list of all 
+my private keys and make them long
