0f23d649852dfb0ea45fc642f58481e36120fcdb
henryqseb <hequijiaso@uide.edu.ec>
2026-03-24 22:45:50 +0000

    It's used to verify if my colleague's key was imported.

--- block_B.sh
+++ block_B.sh
@@ -1,3 +1,4 @@
 gpg --armor --export chineado12@gmail.com > mi_llave_publica.asc
 // It is used to create a new file in which my public key is generated.
-gpg import MICOMPA_llave_publica.asc
+gpg --import MICOMPA_llave_publica.asc //It's used to import my colleague's public key
+gpg --list-keys // It's used to verify if my colleague's key was imported.
