0e25e1f4ad15a2831dbc5a2f98780dfbad3f71af
henryqseb <hequijiaso@uide.edu.ec>
2026-03-25 00:53:25 +0000

    This creates a separate signature

--- block_D.sh
+++ block_D.sh
@@ -1,3 +1,3 @@
 gpg --clearsign doc_no_cifrado.txt // sign the text clearly
 gpg --sign doc_no_cifrado.txt // sign the document in binary format
-gpg --detach-sign doc_no_cifrado.txt //
+gpg --detach-sign doc_no_cifrado.txt // This creates a separate signature
