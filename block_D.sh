2abb8274e87e728738cc7cea3d24f906b943ac3b
henryqseb <hequijiaso@uide.edu.ec>
2026-03-25 01:12:17 +0000

    verify the signatures received

--- block_D.sh
+++ block_D.sh
@@ -1,3 +1,4 @@
 gpg --clearsign doc_no_cifrado.txt // sign the text clearly
 gpg --sign doc_no_cifrado.txt // sign the document in binary format
 gpg --detach-sign doc_no_cifrado.txt // This creates a separate signature
+gpg --verify // verify the signatures received
