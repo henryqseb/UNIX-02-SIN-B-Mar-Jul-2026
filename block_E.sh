d069b5a08f4c071fc72efade8ced0b9264fbf9c8
henryqseb <hequijiaso@uide.edu.ec>
2026-03-26 00:50:03 +0000

    gpg verify the signed document

--- block_E.sh
+++ block_E.sh
@@ -3,3 +3,4 @@ menu that allows you to manage and modify the properties of a key.
 trust // the level of trust we have in our colleague
 gpg --sign-key // sign our colleague's key
 gpg --verify // verify the signed document
+gpg --verify.gpg // verify the signed document
