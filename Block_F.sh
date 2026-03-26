0770db923dc0259baa47e07cc9999a3964062a49
henryqseb <hequijiaso@uide.edu.ec>
2026-03-26 03:03:14 +0000

    Check the file contents

--- Block_F.sh
+++ Block_F.sh
@@ -1,3 +1,4 @@
 gpg --output mensaje_seguro.gpg --encrypt --sign --recipient sakabenalcazar@gmail.com doc_no_cifrado.txt
 // Create a new file using my private key and my partner's public key.
 gpg --decrypt // It is used to decrypt a file
+cat // Check the file contents
