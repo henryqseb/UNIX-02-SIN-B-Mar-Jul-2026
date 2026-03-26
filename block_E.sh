f4dc0fbb890265fd0d27da6c2309adcc870d6b12
henryqseb <hequijiaso@uide.edu.ec>
2026-03-26 00:43:41 +0000

    sign our colleague's key

--- block_E.sh
+++ block_E.sh
@@ -1,4 +1,4 @@
 gpg --edit-key // It serves to open an interactive 
 menu that allows you to manage and modify the properties of a key.
 trust // the level of trust we have in our colleague
-
+gpg --sign-key // sign our colleague's key
