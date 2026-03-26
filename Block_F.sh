baf0c2d4ec72fa757df9e365c9a3e304416894d1
henryqseb <hequijiaso@uide.edu.ec>
2026-03-26 02:57:25 +0000

    It is used to decrypt a file

---
+++ Block_F.sh
@@ -0,0 +1,3 @@
+gpg --output mensaje_seguro.gpg --encrypt --sign --recipient sakabenalcazar@gmail.com doc_no_cifrado.txt
+// Create a new file using my private key and my partner's public key.
+gpg --decrypt // It is used to decrypt a file
--- block_E.sh
+++ block_E.sh
@@ -4,4 +4,4 @@ trust // the level of trust we have in our colleague
 gpg --sign-key // sign our colleague's key
 gpg --verify // verify the signed document
 gpg --verify.gpg // verify the signed document
-gpg --verify doc_no_cifrado.sig doc_no cifrado.txt //verify the signed document
+gpg --verify doc_no_cifrado.sig doc_no_cifrado.txt //verify the signed document
