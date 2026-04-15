6d1859ba0e7c45676a92256b5d1bce63e273edbd
henryqseb <hequijiaso@uide.edu.ec>
2026-04-15 13:10:39 +0000

    shows me the command

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -2,3 +2,5 @@ echo '#!/bin/sh' > hola.sh
 #This command generates another script with the name we gave it, in this case hola.sh
 echo 'echo "Hola desde mi primer script"' >> hola.sh
 #Write hello this is my first script in the file generated with the previous command
+cat hola.sh
+#It reads and shows me the command which was saved in the file
