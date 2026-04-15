9982001bf91db2489b7e7b295f352e39cd86fc4f
henryqseb <hequijiaso@uide.edu.ec>
2026-04-15 13:07:09 +0000

    Write the text in the new file

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,2 +1,4 @@
 echo '#!/bin/sh' > hola.sh
 #This command generates another script with the name we gave it, in this case hola.sh
+echo 'echo "Hola desde mi primer script"' >> hola.sh
+#Write hello this is my first script in the file generated with the previous command
