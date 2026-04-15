2ab00932d3b739a4cea754d6c45568dd90f8c133
henryqseb <hequijiaso@uide.edu.ec>
2026-04-15 13:12:55 +0000

    Access denied result

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -4,3 +4,5 @@ echo 'echo "Hola desde mi primer script"' >> hola.sh
 #Write hello this is my first script in the file generated with the previous command
 cat hola.sh
 #It reads and shows me the command which was saved in the file
+./hola.sh
+#Our command is not executed, resulting in an access denied result.
