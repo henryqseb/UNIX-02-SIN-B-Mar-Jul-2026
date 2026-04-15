2939d94267535873967ad75168515de1b5d30c08
henryqseb <hequijiaso@uide.edu.ec>
2026-04-15 12:54:54 +0000

    sudo and echo :root

---
+++ scripth.sh
@@ -0,0 +1,20 @@
+ls -a and ls --all
+
+ls -l -a -h
+ls -l -ah
+ls -lah
+
+mkdir -- -rf
+rmdir -- -rf
+
+ls --help
+man ls
+man git-clone
+--depth
+
+chmod
+chmod
+chmod
+chmod
+
+sudo echo "hola" > /etc/archivo_protegido #No doesnt work becuase it need other sudo for the 
