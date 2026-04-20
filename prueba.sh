ca12258525f3d977c3f3061f2f253c81a70a2ebe
henryqseb <hequijiaso@uide.edu.ec>
2026-04-20 14:00:23 +0000

    history

--- prueba.sh
+++ prueba.sh
@@ -10,3 +10,54 @@ Ejercico 3
 
 Ejercicio 4
 ejercico 5
+1  git re v
+    2  git re -ve
+    3  git re -v
+    4  git git re -v
+    5  git re -v
+    6  git remote -v
+    7  git fech upstream
+    8  git fetch upstream
+    9  git checkout -b eval-_p2_1_v1 upstream/eval_p2_1_v1
+   10  git push -u origin eval_p2_1_v1
+   11  git push -u original eval_p2_1_v1
+   12  git push -u origin  eval_p2_1_v1
+   13  git push -u origin eval_p2_1_v1
+   14  git mv nebula/registros nebula/bitacoras
+   15  ls nebula/
+   16  mv nebula/registros nebula/bitacoras
+   17  ls -l nebula/
+   18  git push -u origin eval_p2_1_v1
+   19  git remote -v
+   20  git fetch upstream
+   21  git tetch upstream
+   22  git fetch upstream
+   23  git checkout -b eval_p2_1_v1 upstream/eval_p2_1_v1
+   24  git push -u origin eval_p2_1_v1
+   25  git mv nebula/registros nebula/bitacoras
+   26  mv nebula/registros nebula/bitacoras
+   27  ls -l nebula/
+   28  find . -type d -name "registros"
+   29  mv registros bitacoras
+   30  git status
+   31  ls -la nebula/
+   32  mkdir nebula/bitacoras nebula/borradores
+   33  ls -la nebula/
+   34  mv nebula cosmos/cosmos.txt nebula/bitacoras/
+   35  mv nebula/cosmos.txt nebula/bitacoras/
+   36  mv nebula/estrella.bak nebula/estrella.conf
+   37  git add.
+   38  git add .
+   39  chmod 640 nebula/bitacoras/cosmos.txt
+   40  git add .
+   41  chmod u+x nebula/galaxia.sh
+   42  chmod o-w nebula/estrella.conf
+   43  chmod u+s nebula/galaxia.sh
+   44  ls -l nebula/galaxia.sh
+   45  git add .
+   46  chmod +t /tmp/nebula_zone
+   47  mkdir /tmp/nebula_zone
+   48  chmod 777 /tmp/nebula_zone
+   49  chmod +t /tmp/nebula_zone
+   50  ls -ld /tmp/nebula_zone
+   51  history
