635bfdbcc283ff4e0d4df88aa7a1473cba7cf62c
henryqseb <hequijiaso@uide.edu.ec>
2026-04-27 13:25:36 +0000

    UMASK 022

--- hola.sh
+++ hola.sh
@@ -13,4 +13,14 @@ umask 077
 touch secreto.txt
 mkdir privado
 #Commands
+#Part 4
+umask 022
+whoami
+echo "Hola">mi_archivo
+ls-l mi_archivo
+#These commands configure default permissions, 
+#identify the user, create a text file, and display its detailed attributes.
 
+sudo useradd -m -s /usr/bin/zsh luna
+sudo chown luna mi_arcihvo
+ls -l mi_archivo
