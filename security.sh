d9fc5938ac0bbb9bb0fa093b6019e93fdb4edf4d
henryqseb <hequijiaso@uide.edu.ec>
2026-05-04 12:51:43 +0000

    id and cat

---
+++ security.sh
@@ -0,0 +1,7 @@
+#Commands
+id
+#display the actual and 
+#effective user and group identifiers for the current session.
+cat /etc/passwd | head -10
+#Displays the first 10 lines of /etc/passwd, the file that lists system 
+#accounts, their IDs, home folders, and startup shells.
