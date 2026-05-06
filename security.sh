bdf2851de5425d31ef21a040acf7e846c5fcc018
henryqseb <hequijiaso@uide.edu.ec>
2026-05-06 12:52:12 +0000

    addgroup diseno

--- security.sh
+++ security.sh
@@ -48,3 +48,12 @@ grep "GID_MIN\|GID_MAX\|SYS_GID" /etc/login.defs
 #SYS_GID_MAX = 999
 #GID_MIN = 1000
 #GID_MAX = 60000
+
+#Basic Sintax
+#addgroup [opciones] nombre_grupo
+#create groups with addgroup
+sudo addgroup diseno
+sudo addgroup --gid 2100 marketing
+sudo addgroup --system cache_web
+#Verify
+grep "diseno\|marketing\|cache_web" /etc/group
