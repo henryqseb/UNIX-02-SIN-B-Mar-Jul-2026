ee82a8c277bc28ccfd9fcd2342b291a266b927a1
henryqseb <hequijiaso@uide.edu.ec>
2026-05-27 13:00:08 +0000

    Classwork: First commit

---
+++ security2.sh
@@ -0,0 +1,7 @@
+#See the current main group
+id
+id -gn #only the name of the main group
+#create an archive and see which group I inherited
+touch ~/test_grupo_heredero.txt
+ls -la ~/test_grupo_heredero.txt
+#the group is the user's primary group
