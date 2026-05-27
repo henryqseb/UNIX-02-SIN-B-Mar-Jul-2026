6d29214e3bc00ed4fe4a0b36783714f3a4dae864
henryqseb <hequijiaso@uide.edu.ec>
2026-05-27 13:09:08 +0000

    second commit to classwork

--- security2.sh
+++ security2.sh
@@ -5,3 +5,9 @@ id -gn #only the name of the main group
 touch ~/test_grupo_heredero.txt
 ls -la ~/test_grupo_heredero.txt
 #the group is the user's primary group
+echo "Grupo actual: $(id-gn")
+
+#create the file before the newgrp
+
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
