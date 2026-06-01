fc029d173aac3498acc8d6f139a27f9c394f734f
henryqseb <hequijiaso@uide.edu.ec>
2026-06-01 13:16:10 +0000

    bash blackhatbash1.sh

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -8,3 +8,6 @@ ps -e -f
 ps -ef
 df --human-readable
 df -h
+#bash blackhatbash1.sh
+#The command executes an automated sequence of instructions 
+#defined in the blackhatbash1.sh file using the Bash interpreter to perform specific administration or security tasks.
