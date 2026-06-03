ca2b9a8bd449a6fbb72f9be3a51a96b8f2c71817
henryqseb <hequijiaso@uide.edu.ec>
2026-06-03 13:34:28 +0000

    cat echo >>, < &, ls

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -6,3 +6,19 @@ touch test && touch test123
 ls; ps; whoami
 
 lzl || echo "el comando lzl fallo"
+
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / l> stdout.txt 2>stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout_and_stderr.txt
+lzl 2> error.txt
+ cat error.txt
+
+ echo "uno-uno" > archivo.txt
+cat archivo.txt
+echo "dos-dos" >> archivo.txt
+cat archivo.txt
+echo "tres" &> archivo.txt
+cat archivo.txt
+echo "tres" &>> archivo.txt
