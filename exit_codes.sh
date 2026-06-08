626e04c0841e28d5360486be80367a81ffcca1b9
henryqseb <hequijiaso@uide.edu.ec>
2026-06-08 13:24:49 +0000

    Exiting with exit code: 0

--- exit_codes.sh
+++ exit_codes.sh
@@ -6,3 +6,11 @@ echo "The exit code of the ls command was: $?"
 lzl 2> /dev/null
 echo "The exit code of the non-existing lzl command was: $?"
 Listing 1-
+
+# Sets the exit code of the script to be 223
+echo "Exiting with exit code: 223"
+exit 223
+
+$ ps -ef
+$ echo $?
+0
