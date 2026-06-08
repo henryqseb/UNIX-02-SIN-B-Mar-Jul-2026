74cd3548fff8c9592c33ac689e0e6e9c92b7e406
henryqseb <hequijiaso@uide.edu.ec>
2026-06-08 13:12:17 +0000

    echo "NAME" and "LASTNAME"

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -7,7 +7,6 @@ echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
 ping -c 3 "${TARGET}"
 
-
 echo "The arguments are: $@"
 echo "The total number of arguments is: $#"
 
