d76d794938bbc7e68008cdb30fe100f735f73c72
henryqseb <hequijiaso@uide.edu.ec>
2026-06-08 12:51:49 +0000

    The 3 pings appear with the count

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -5,6 +5,8 @@ SCRIPT_NAME="${0}"
 TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
-ping "${TARGET}"
+ping -c 3 "${TARGET}"
 
-bash -x ping_with_arguments.sh 127.0.0.1
+
+echo "The arguments are: $@"
+echo "The total number of arguments is: $#"
