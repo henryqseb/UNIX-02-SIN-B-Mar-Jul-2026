f1743d892d99b5334a65806094ef64ab19da90e2
henryqseb <hequijiaso@uide.edu.ec>
2026-06-10 12:33:30 +0000

    if [[! -f "${FILENAME}"]]; then touch "${FILENAME}" fi

--- test_if_file_exists.sh
+++ test_if_file_exists.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 FILENAME="flow_control_with_if.txt"
-if [[ -f "${FILENAME}" ]]; then
-    echo "${FILENAME} already exists;."
-    exit 1
-else
-touch "{FILENAME}"
+
+if [[ ! -f "${FILENAME}" ]]; then
+touch "${FILENAME}"
 fi
